efe7b95d1e1f081915a4efbd692be971d655f0c5
UNS2406 <71777971+UNS2406@users.noreply.github.com>
2022-07-13 22:41:30 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import React from "react";
-
+/* 
+our component created so to render it we’ll import it in the app.js file 
+and pass it inside the render function of app.js
+*//**/
 import PickImage from "./screens/Camera.js";
 
 export default class App extends React.Component {
